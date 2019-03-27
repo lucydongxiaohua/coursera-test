8dd2688a387f85efb3cb851c9de496204fdc92b3
lucydongxiaohua <44466718+lucydongxiaohua@users.noreply.github.com>
2019-03-27 10:51:51 +0800

    Add files via upload

---
+++ BrinaryNum.java
@@ -0,0 +1,47 @@
+package h2;
+
+public class BrinaryNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// IntToBinary(10);
+		// Inverse(IntToBinary(20));
+		number(Inverse(IntToBinary(20)));
+
+	}
+
+	public static String IntToBinary(int value) {
+
+		String str = "";
+		while (value != 0) {
+			str += (value % 2);
+			value = value / 2;
+
+		}
+		System.out.println(str);
+		return str;
+	}
+
+	public static String Inverse(String str) {
+		String newStr = "";
+		for (int i = str.length() - 1; i >= 0 && i < str.length(); i--) {
+			newStr += str.substring(i, i + 1);
+		}
+		System.out.println("newStr" + newStr);
+		return newStr;
+	}
+
+	public static int number(String str) {
+		int x = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (str.substring(i, i + 1).equals("1")) {
+				++x;// 先自加后赋值；x++先赋值后自加
+			}
+
+		}
+		System.out.println("x:" + x);
+		return x;
+
+	}
+
+}
